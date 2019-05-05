94467d9bcc2511dc0114adf04c54e4cde6c178b7
YinYihan <Koixxx1124@gmail.com>
2019-05-05 20:05:54 +0800

    remove test file

--- src/main/java/com/spring/demo/repository/test/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.demo.repository.test;
-
-import com.spring.demo.entity.test.Login;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login,Integer> {
-
-}
